20ffea2304f613616eaf4e023da675d0647208df
Jon <haygoodjon@gmail.com>
2014-11-04 10:05:49 -0800

    Fix circles code

--- circles/my.js
+++ circles/my.js
@@ -2,42 +2,38 @@
   var data = [[5,5], [5,5], [5,5], [5,5], [5,5]];
   var svg = d3.select('svg');
 
-  var circle = svg.selectAll("circle");
-  circle
-    .data(data, function(d, i){ console.log(d); return d[i];})
-  .enter().append("circle")
-    .attr("cy", 60)
-    .attr("cx", function(d, i) { return i * 100 + 30; })
-    .attr("r", function(d) { return d[0] * d[1]; })
-    .style("fill", "steelblue");
+  var update = (function me(data){
+    var circle = svg.selectAll("circle").data(data);
+    circle
+      .attr("cy", function(d, i){ return i * 100 + d[1] * 1;})
+      .attr("cx", function(d, i) { return i * 100 + d[0] * 1; })
+      .attr("r", function(d, i) { return Math.sqrt(d[0] * d[1]); })//d[0] * d[1]; })
+    .enter().append("circle")
+      .attr("cy", function(d, i){ return 40;})
+      .attr("cx", function(d, i) { return i * 100 + 30; })
+      .attr("r", function(d, i) { return Math.sqrt(d[0] * d[1]); })//d[0] * d[1]; })
+      .style("fill", "steelblue");
+    return me;
+  })(data);
 
-  var arr = [];
-  circle.each(function(d, i){
-    arr.push(d3.select(this));
-  })
+  var inc = 1;
   function compare(current){
-    if(current > 200){
-      return -Math.random() * 10;
-    }else if (current < 20){
-      return Math.random() * 10;
+    if(current > 50){
+      inc = - Math.random() * 10;
+    }else if (current <= 5){
+      inc = Math.random() * 10;
     }
+    return (current + inc) > 0 ? current + inc : 0  ;
   }
-  var i = 0, len = arr.length;
+
   setInterval(function(){
-    console.log(circle.data());
-    //data = [[15,15], [15,15], [15,15], [15,15], [15,15]];
-    // for(i = 0; i < len; i++){
-    //   arr[i].attr('cx', function(d){
-    //     d[0] = compare(+arr[i].attr('cx')) || d[0];
-    //     return +arr[i].attr('cx') + d[0];
-    //   });
-    //
-    //   arr[i].attr('cy', function(d){
-    //     d[1] = compare(+arr[i].attr('cy')) || d[1];
-    //     return +arr[i].attr('cy') + d[1];
-    //   });
-    // }
-  }, 4000);
+    data = [[compare(data[0][0]),compare(data[0][1])],
+            [compare(data[1][0]),compare(data[1][1])],
+            [compare(data[2][0]),compare(data[2][1])],
+            [compare(data[3][0]),compare(data[3][1])],
+            [compare(data[4][0]),compare(data[4][1])]];
+    update(data);
+  }, 200);
 
 
 })();
